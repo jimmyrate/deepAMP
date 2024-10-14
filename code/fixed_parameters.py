93b725abd3b6a4dec99afd366cf91a803012d2f0
jimmyrate <57659929+jimmyrate@users.noreply.github.com>
2024-10-14 19:27:09 +0800

    Add files via upload

---
+++ code/fixed_parameters.py
@@ -0,0 +1,11 @@
+SPACIAIL_TAG = {'[PAD]':'#',
+                '[CLS]':'$',
+                '[SEP]':'&',
+                '[MASK]':'*'}
+
+# SPECULATED_EPOCH = (349+1,)
+# SPECULATED_EPOCH = (199+1,)
+# SPECULATED_EPOCH = tuple(range(180+1,200+1))
+# SPECULATED_EPOCH = tuple(range(20+1,40+1))
+# SPECULATED_EPOCH = tuple(range(480+1,500+1))
+SPECULATED_EPOCH = tuple(range(180+1,200+1))
